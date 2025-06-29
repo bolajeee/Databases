d482e766fc1e164c73ef92d0d0927749e625dacc
bolajeee <ibrahimomobolaji1999@gmail.com>
2025-06-29 14:24:04 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 -- INSERT A SINGLE CUSTOMER INTO THE CUSTOMERS TABLE
-INSERT INTO Customer (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
