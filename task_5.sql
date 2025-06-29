53c32f05c5f64b408c414f8ca2a5557fd8c97663
bolajeee <ibrahimomobolaji1999@gmail.com>
2025-06-29 14:18:15 +0100

    Create task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- INSERT A SINGLE CUSTOMER INTO THE CUSTOMERS TABLE
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
